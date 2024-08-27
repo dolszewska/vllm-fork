4e78a3add980164f72636e9da7cd52ca40cf8451
Dominika Olszewska <dolszewska@habana.ai>
2024-08-27 13:42:28 +0300

    disable tensor cache 2.0

--- vllm/worker/habana_model_runner.py
+++ vllm/worker/habana_model_runner.py
@@ -1362,7 +1362,7 @@ class HabanaModelRunnerBase(ModelRunnerBase[TModelInputForHPU]):
 def _maybe_wrap_in_hpu_graph(*args, **kwargs):
     return htorch.hpu.wrap_in_hpu_graph(HpuModelAdapter(
         *args, **
-        kwargs)) if htorch.utils.internal.is_lazy() else HpuModelAdapter(
+        kwargs), disable_tensor_cache=True) if htorch.utils.internal.is_lazy() else HpuModelAdapter(
             *args, **kwargs)
 
 
