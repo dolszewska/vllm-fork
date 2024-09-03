1e6aed0e5e682bc607a65120b463a0fc414ea37c
Dominika Olszewska <dolszewska@habana.ai>
2024-09-03 11:48:00 +0300

    print input view hash

--- vllm/worker/habana_model_runner.py
+++ vllm/worker/habana_model_runner.py
@@ -15,6 +15,7 @@ from typing import (TYPE_CHECKING, Any, Callable, Dict, List, NamedTuple,
                     Optional, Set, Tuple, Type, TypeVar, Union)
 
 import habana_frameworks.torch as htorch
+from habana_frameworks.torch import _hpu_C
 import torch
 
 from vllm.attention import AttentionMetadata, get_attn_backend
@@ -1539,13 +1540,14 @@ class HabanaModelRunner(
         input_hash=htorch.hpu.graphs.input_hash(execute_model_kwargs)
         input_hash_metadata=htorch.hpu.graphs.input_hash(trimmed_attn_metadata)
         input_hash_input_ids=htorch.hpu.graphs.input_hash(input_tokens)
+        input_view_hash_input_ids=_hpu_C.get_view_hash(input_tokens)
         input_hash_positions=htorch.hpu.graphs.input_hash(input_positions)
         input_hash_kv_caches=htorch.hpu.graphs.input_hash(kv_caches)
         input_hash_intermediate_tensors=htorch.hpu.graphs.input_hash(intermediate_tensors)
         
         print()
         print(
-            f"Is prompt: {is_prompt}, Free memory: {free_mem}, Input hash: {input_hash}, Trimmed metadata: {input_hash_metadata},\n"
+            f"Is prompt: {is_prompt}, Free memory: {free_mem}, Input hash: {input_hash}, Trimmed metadata: {input_hash_metadata}, Input tokens view hash: {input_view_hash_input_ids}\n"
             f"Input ids: {input_hash_input_ids}, Positions: {input_hash_positions}, KV caches: {input_hash_kv_caches}, Intermediate tensors: {input_hash_intermediate_tensors}\n"
             f"Batch size: {batch_size}, Seq length: {seq_len}, Input tokens shape: {input_tokens.shape}, Input tokens: {input_tokens}",
             end=""
